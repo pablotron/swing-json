fc92e1b6d48b8a06f1f81c3f9235f623b3a7e3aa
Paul Duncan <pabs@pablotron.org>
2015-12-02 19:51:24 -0500

    add button support

--- src/org/pablotron/swingjson/ContextParser.java
+++ src/org/pablotron/swingjson/ContextParser.java
@@ -7,8 +7,12 @@ import java.util.Map;
 import java.util.HashMap;
 
 import java.awt.Component;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
 
 import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
@@ -58,6 +62,17 @@ public final class ContextParser {
           );
         }
 
+        // set default button
+        if (el.has("default-button")) {
+          final String id = el.get("default-button").getAsString();
+
+          context.addInit(new Runnable() {
+            public void run() {
+              frame.getRootPane().setDefaultButton((JButton) context.get(id));
+            }
+          });
+        }
+
         // add pack runnable
         context.addInit(new Runnable() {
           public void run() {
@@ -85,7 +100,97 @@ public final class ContextParser {
         final Context context
       ) throws Exception {
         final String text = el.has("text") ? el.get("text").getAsString() : "";
-        return new JLabel(context.getText(text));
+        final JLabel r = new JLabel(context.getText(text));
+
+        init_component(el, r, context);
+
+        return r;
+      }
+    },
+
+    BUTTON("button") {
+      private int parse_text_position(final String s) throws Exception {
+        switch (s) {
+        case "bottom":
+          return javax.swing.SwingConstants.BOTTOM;
+        case "center":
+          return javax.swing.SwingConstants.CENTER;
+        case "east":
+          return javax.swing.SwingConstants.EAST;
+        case "horizontal":
+          return javax.swing.SwingConstants.HORIZONTAL;
+        case "leading":
+          return javax.swing.SwingConstants.LEADING;
+        case "left":
+          return javax.swing.SwingConstants.LEFT;
+        case "next":
+          return javax.swing.SwingConstants.NEXT;
+        case "north":
+          return javax.swing.SwingConstants.NORTH;
+        case "north-east":
+          return javax.swing.SwingConstants.NORTH_EAST;
+        case "north-west":
+          return javax.swing.SwingConstants.NORTH_WEST;
+        case "previous":
+          return javax.swing.SwingConstants.PREVIOUS;
+        case "right":
+          return javax.swing.SwingConstants.RIGHT;
+        case "south":
+          return javax.swing.SwingConstants.SOUTH;
+        case "south-east":
+          return javax.swing.SwingConstants.SOUTH_EAST;
+        case "south-west":
+          return javax.swing.SwingConstants.SOUTH_WEST;
+        case "top":
+          return javax.swing.SwingConstants.TOP;
+        case "trailing":
+          return javax.swing.SwingConstants.TRAILING;
+        case "vertical":
+          return javax.swing.SwingConstants.VERTICAL;
+        case "west":
+          return javax.swing.SwingConstants.WEST;
+        default:
+          throw new Exception("unknown position: " + s);
+        }
+      }
+
+      public Component make(
+        final JsonObject el,
+        final Context context
+      ) throws Exception {
+        final String text = el.has("text") ? el.get("text").getAsString() : "";
+        // TODO: icon
+        final JButton b = new JButton(context.getText(text));
+
+        // init common component attributes
+        init_component(el, b, context);
+
+        // add action
+        if (el.has("action")) {
+          b.setActionCommand(el.get("action").getAsString());
+        }
+
+        // add mnemonic
+        if (el.has("mnemonic")) {
+          b.setMnemonic(KeyEvent.getExtendedKeyCodeForChar(el.get("mnemonic").getAsString().codePointAt(0)));
+        }
+
+        // add vertical text position
+        if (el.has("vertical-text-position")) {
+          b.setVerticalTextPosition(parse_text_position(
+            el.get("vertical-text-position").getAsString()
+          ));
+        }
+
+        // add horizontal text position
+        if (el.has("horizontal-text-position")) {
+          b.setHorizontalTextPosition(parse_text_position(
+            el.get("horizontal-text-position").getAsString()
+          ));
+        }
+
+        // return button
+        return b;
       }
     };
 
@@ -119,6 +224,25 @@ public final class ContextParser {
       throw new Exception("unknown type: " + type);
 
     return types.get(type);
+  }
+
+  /*
+   * init common component properties
+   */
+  private static void init_component(
+    final JsonObject el,
+    final JComponent r,
+    final Context context
+  ) throws Exception {
+    // add tooltip
+    if (el.has("tip")) {
+      r.setToolTipText(context.getText(el.get("tip").getAsString()));
+    }
+
+    // add enabled
+    if (el.has("enabled")) {
+      r.setEnabled(el.get("enabled").getAsBoolean());
+    }
   }
 
   private static Component make(
