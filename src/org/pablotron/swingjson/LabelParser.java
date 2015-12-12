cb1981ccdbbdb861c1891ef70e4cf16a903fad8a
Paul Duncan <pabs@pablotron.org>
2015-12-12 00:19:20 -0500

    add layout text-position and label-align support

--- src/org/pablotron/swingjson/LabelParser.java
+++ src/org/pablotron/swingjson/LabelParser.java
@@ -16,6 +16,31 @@ public final class LabelParser implements ComponentParser {
 
     ComponentParsers.init_component(context, el, r);
 
+    if (el.has("text-position")) {
+      final JsonObject o = el.getAsJsonObject("text-position");
+
+      if (o.has("vertical"))
+        r.setVerticalTextPosition(get_position(o, "vertical"));
+      if (o.has("horizontal"))
+        r.setHorizontalTextPosition(get_position(o, "horizontal"));
+    }
+
+    if (el.has("label-align")) {
+      final JsonObject o = el.getAsJsonObject("label-align");
+
+      if (o.has("vertical"))
+        r.setVerticalAlignment(get_position(o, "vertical"));
+      if (o.has("horizontal"))
+        r.setHorizontalAlignment(get_position(o, "horizontal"));
+    }
+
     return r;
+  }
+
+  private static int get_position(
+    final JsonObject o,
+    final String key
+  ) throws Exception {
+    return TextPosition.parse(o.get(key).getAsString());
   }
 };
