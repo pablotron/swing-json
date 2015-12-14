ac570eb93dfd3de5ac9d3c66aba25c881f20a368
Paul Duncan <pabs@pablotron.org>
2015-12-13 21:40:53 -0500

    refactor borderlayoutpositions

--- src/org/pablotron/swingjson/BorderLayoutPositions.java
+++ src/org/pablotron/swingjson/BorderLayoutPositions.java
@@ -3,22 +3,23 @@ package org.pablotron.swingjson;
 import java.util.Map;
 import java.util.HashMap;
 import java.awt.BorderLayout;
+import static java.awt.BorderLayout.*;
 import com.google.gson.JsonObject;
 
 public final class BorderLayoutPositions {
   private BorderLayoutPositions() {}
 
   private static Map<String, String> lut = new HashMap<String, String>() {{
-    put("center", BorderLayout.CENTER);
-    put("line-start", BorderLayout.LINE_START);
-    put("line-end", BorderLayout.LINE_END);
-    put("page-start", BorderLayout.PAGE_START);
-    put("page-end", BorderLayout.PAGE_END);
+    put("center", CENTER);
+    put("line-start", LINE_START);
+    put("line-end", LINE_END);
+    put("page-start", PAGE_START);
+    put("page-end", PAGE_END);
   }};
 
   protected static String get(final JsonObject config) throws Exception {
     if (!config.has("position"))
-      return BorderLayout.CENTER;
+      return CENTER;
 
     final String key = config.get("position").getAsString();
     if (!lut.containsKey(key))
